cd46deaf68bb9386be91ef151d96c1f7193a25bf
Sebastian Koch <koch.sebastian@protonmail.com>
2022-11-07 16:39:35 +0100

    feat: add logger

--- src/main/java/mat/agent/reactive/App.java
+++ src/main/java/mat/agent/reactive/App.java
@@ -4,13 +4,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import mat.agent.reactive.model.Warehouse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Optional;
 
 public class App extends javafx.application.Application {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Starting application..");
         // Optional<Warehouse> warehouse = ConfigLoader.getWarehouseSetup();
 
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module mat.agent.reactive {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires org.apache.logging.log4j;
 
     opens mat.agent.reactive to javafx.fxml;
     exports mat.agent.reactive;
