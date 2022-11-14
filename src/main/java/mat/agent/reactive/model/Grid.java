ecc76aaa1a61d37384b0bd1e48cb0d3d127b8658
leonmoll <leon.m.moll@campus.tu-berlin.de>
2022-11-14 19:32:09 +0100

    grid ready to be copied into python script

---
+++ src/main/java/mat/agent/reactive/model/Grid.java
@@ -0,0 +1,188 @@
+package mat.agent.reactive.model;
+
+
+
+public class Grid {
+	
+	public int size_x;
+	public int size_y;
+	public int[][] matrix;
+	enum PPositions {
+		NONE,
+		RANDOM_BORDER,
+		RANDOM,
+		NEAREST_BORDER,
+		DISTRIBUTED_BORDER
+	}
+	PPositions pPosition;
+	public int numberOfIdlingZones;
+	public int DROP_ZONE = 9;
+	public int IDLING_ZONE = 5;
+	public int FREE = 0;
+	public int ROBOT= 1;	
+	
+	
+	
+	
+	public Grid (int size_x, int size_y, PPositions pPosition , int numberOfIdlingZones) {
+		this.size_x = size_x;
+		this.size_y = size_y;
+		this.matrix = new int[size_x][size_y]; 
+		this.pPosition = pPosition;
+		this.numberOfIdlingZones = numberOfIdlingZones;
+		this.init();
+		this.printMatrix();
+		
+
+	}
+	
+	public void init() {
+		int currentIdlingZones = 0;
+//		drop of zones
+		for (int i = 0; i < this.size_x/2+1; i++) {
+			matrix[0][i*2]= DROP_ZONE;
+		}
+//			
+
+//		fill in parking positions
+		switch (pPosition) {
+		case NONE:
+			break;
+		case DISTRIBUTED_BORDER:
+			for (int x = 1; x<size_x/2+1; x++) {
+				if(currentIdlingZones<numberOfIdlingZones) {
+					matrix[x*2][0] = IDLING_ZONE;
+					currentIdlingZones++;
+				}
+				if(currentIdlingZones<numberOfIdlingZones) {
+					matrix[x*2][size_y-1] = IDLING_ZONE;
+					currentIdlingZones++;
+				}
+			}
+//			fill bottom if needed
+			for (int y = 1; y<size_y/2+1; y++) {
+				if(currentIdlingZones<numberOfIdlingZones) {
+					matrix[size_x-1][y*2] = IDLING_ZONE;
+					currentIdlingZones++;
+				}					
+			}
+			if(currentIdlingZones<numberOfIdlingZones) {
+				System.err.println("more idling zones than " + currentIdlingZones+ " are not possible");
+			}
+
+			break;
+		case NEAREST_BORDER:
+//			fill top
+			for (int x = 1; x<size_x/2+1; x++) {
+				if(currentIdlingZones<numberOfIdlingZones) {
+					matrix[0][x*2-1] = IDLING_ZONE;
+					currentIdlingZones++;
+				}
+			}
+				
+//			fill sides if needed
+				for (int x = 2; x<size_y; x++) {
+					if(currentIdlingZones<numberOfIdlingZones) {
+						matrix[x][0] = IDLING_ZONE;
+						currentIdlingZones++;
+					}
+					if(currentIdlingZones<numberOfIdlingZones) {
+						matrix[x][size_y-1] = IDLING_ZONE;
+						currentIdlingZones++;
+					}
+				}
+				
+//				fill bottom if needed
+				for (int y = 1; y<size_y; y++) {
+					if(currentIdlingZones<numberOfIdlingZones) {
+						matrix[size_x-1][y] = IDLING_ZONE;
+						currentIdlingZones++;
+					}	
+				}
+				if(currentIdlingZones<numberOfIdlingZones) {
+					System.err.println("more idling zones than " + currentIdlingZones+ " are not possible");
+				}						
+			break;
+		case RANDOM:
+			while(currentIdlingZones<numberOfIdlingZones) {
+				int random_x = (int)Math.floor(Math.random()*(size_x)); //random number between 0 and size_x
+				System.out.println(random_x);
+				int random_y = (int)Math.floor(Math.random()*(size_y));
+				System.out.println(random_y);
+				if (matrix[random_x][random_y]!=DROP_ZONE && matrix[random_x][random_y]!=IDLING_ZONE) {
+					matrix[random_x][random_y] = IDLING_ZONE;
+					currentIdlingZones++;
+	 				}
+			}
+			break;
+		case RANDOM_BORDER:
+//			first check if the border space is big enough
+			int maxNumberOfIdlingZones=(((size_x-1)/2))+(2*(size_y-2))+size_x-2;
+			System.out.println(maxNumberOfIdlingZones);
+			if (numberOfIdlingZones>maxNumberOfIdlingZones) {
+				System.err.println("more idling zones than " + maxNumberOfIdlingZones + " are not possible");
+			}
+			while(currentIdlingZones<numberOfIdlingZones) {				
+				//first decide on which side the idling zone should go
+				int random_side = (int)Math.floor(Math.random()*(4));
+				switch(random_side) {
+//				top:
+				case 1:
+					int y = (int)Math.floor(Math.random()*size_x); //random number which cant be a corner
+					if (matrix[0][y]!=DROP_ZONE && matrix[0][y]!=IDLING_ZONE && y<size_y-1){
+						matrix[0][y] = IDLING_ZONE;
+						currentIdlingZones++;
+		 				}
+//				right
+				case 2: 
+					int x = (int)Math.floor(Math.random()*size_y);
+					if (matrix[x][size_y-1]!=DROP_ZONE && matrix[x][size_y-1]!=IDLING_ZONE && x<size_x-1){
+						matrix[x][size_y-1] = IDLING_ZONE;
+						currentIdlingZones++;
+		 				}
+//				bottom	
+				case 3: 
+					int y_b = 1 + (int)(Math.random() * ((size_y - 3) + 1)); //to make sure its not filling the corners
+					if (matrix[size_x-1][y_b]!=DROP_ZONE && matrix[size_x-1][y_b]!=IDLING_ZONE){
+						matrix[size_x-1][y_b] = IDLING_ZONE;
+						currentIdlingZones++;
+		 				}
+//				left
+					
+				case 4: 
+					int x_l = (int)Math.floor(Math.random()*size_y);
+					if (matrix[x_l][0]!=DROP_ZONE && matrix[x_l][0]!=IDLING_ZONE && x_l<size_x-1){
+						matrix[x_l][0] = IDLING_ZONE;
+						currentIdlingZones++;				
+					}
+					
+				
+				
+			}
+		}
+			break;
+			
+		}
+		}
+	
+		
+		
+	public void printMatrix() {
+		
+		for (int x = 0; x < size_x; x++) {
+		    for (int y = 0; y < size_y; y++) {
+		        System.out.print(matrix[x][y] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+	
+//	public static void main(String[] args) {
+//		Grid grid = new Grid(7, 7, PPositions.RANDOM_BORDER, 5);
+//
+//	}
+		
+		
+	
+
+}
